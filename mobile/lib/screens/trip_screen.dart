1042662df4030652d7909925dcaaaaed5b394127
Gowtham <gowtham@Gowthams-MacBook-Air.local>
2026-08-31 02:16:16 +0530

    fix: import foundation in trip_screen.dart

--- mobile/lib/screens/trip_screen.dart
+++ mobile/lib/screens/trip_screen.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'dart:async';
 import 'dart:ui' as ui;
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_map/flutter_map.dart';
